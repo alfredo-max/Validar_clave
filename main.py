3edac48d2c144fc531b2ee1b0c94a8bea912ebc6
alfredo-max <55604099+alfredo-max@users.noreply.github.com>
2020-03-27 22:21:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import re
 patron=re.compile("^[A-Z](\d){3}[a-z]{3}[^\w]{3}$")
-cadena="A608aic?&%"
+cadena=input(" Ingrese la contraseña: ");
 m=patron.match(cadena)
 if m!=None:
    print(f"la contraseña {cadena} es valida")
 else:
     print(f"la contraseña {cadena} NO es valida") 
-
